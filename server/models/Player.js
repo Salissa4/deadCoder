7ba005f9309bc59a154f1e82d91b5715072f824b
GH0ST <adamjamessherrill@gmail.com>
2023-01-15 11:27:59 -0600

    finished player model

--- server/models/Player.js
+++ server/models/Player.js
@@ -1,23 +1,18 @@
-const { Schema, model } = require("mongoose");
-const bcrypt = require("bcrypt");
-
-class Player extends model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+var mongoose = require("mongoose"),
+  Schema = mongoose.Schema,
+  bcrypt = require("bcrypt"),
+  SALT_WORK_FACTOR = 10;
 
 const playerSchema = new Schema({
   user_name: {
     type: String,
     required: true,
-    unique: true,
+    index: { unique: true },
     trim: true,
   },
   password: {
     type: String,
     required: true,
-    unique: true,
     trim: true,
   },
   avatar: {
@@ -30,6 +25,33 @@ const playerSchema = new Schema({
   },
 });
 
-const Player = model("Player", playerSchema);
+playerSchema.pre("save", function (next) {
+  let player = this;
+  // only hash the password if it has been modified (or is new)
+  if (!player.isModified("password")) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password along with our new salt
+    bcrypt.hash(player.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      player.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+  });
+};
+
+const Player = mongoose.model("Player", playerSchema);
 
 module.exports = Player;
