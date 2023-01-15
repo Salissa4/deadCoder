2f0ede8aacb4cba8d94a58f5432a38b9cfe7aaeb
GH0ST <adamjamessherrill@gmail.com>
2023-01-15 11:57:50 -0600

    corrected index.js

--- server/models/HighScores.js
+++
@@ -1,4 +0,0 @@
-const Player = require('./Player');
-const Likes = require('./Likes');
-
-module.exports = { Player, Likes };
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const Player = require('./Player');
+const Likes = require('./Likes');
+
+module.exports = { Player, Likes };
