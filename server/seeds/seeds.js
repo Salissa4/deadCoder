2b3ad35870bb3a9fbdd98b6fff0658dad1222d48
Monahlissa <salissa4@gmail.com>
2023-01-17 14:23:20 -0600

    seeds/seed.js file

--- server/seeds/seeds.js
+++ server/seeds/seeds.js
@@ -3,4 +3,30 @@ const { Player, Scores } = require ('../models');
 const playerSeeds = require ('./playerSeeds.json');
 const scoreSeeds = require ('./scoreSeeds.json');
 
-//TODO call the playerSchema.pre function to hash plain password
+db.once('open', async () => {
+    try {
+      await Scores.deleteMany({});
+      await Player.deleteMany({});
+  
+      await Player.create(playerSeeds);
+        //TODO what do i change thoughtAuthor to?
+      for (let i = 0; i < scoreSeeds.length; i++) {
+        const { _id, thoughtAuthor } = await Score.create(scoreSeeds[i]);
+        const player = await Player.findOneAndUpdate(
+            //TODO is username:player, and addToSet score: _id correct?
+            { username: player },
+            {
+                $addToSet: {
+                score: _id,
+                },
+          }
+        );
+      }
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  
+    console.log('all done!');
+    process.exit(0);
+  });
