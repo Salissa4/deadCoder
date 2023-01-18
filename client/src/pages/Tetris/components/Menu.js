60b384cad2c4d63c2329f685c1785f5cf54968f8
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2023-01-18 09:54:11 -0600

    css for tetris games

--- client/src/pages/Tetris/components/Menu.js
+++ client/src/pages/Tetris/components/Menu.js
@@ -1,11 +1,9 @@
 import './Menu.css'
-
+import { Button } from '@mui/material';
 
 const Menu = ({ onClick }) => (
     <div className="Menu">
-        <button className="Button" onClick={onClick}>
-            Play!
-        </button>
+        <Button className="Button" onClick={onClick} sx={{ width: 200, fontFamily: 'MenloRegular', fontSize:15, color: 'white', border: 0.5, borderColor: '#929292', borderRadius: 0 }}>PLAY</Button>
     </div>
 )
 
