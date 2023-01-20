82e1365d60e1026e0af6ef3bab4ba3de07b55d46
GH0ST <adamjamessherrill@gmail.com>
2023-01-19 20:04:45 -0600

    updated client/utils/queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,45 @@
-//boilerplate
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
+export const QUERY_PLAYER = gql`
+  query Player($username: String!) {
+    player(username: $username) {
       _id
       username
       avatar
+      codingLang
+      scores {
+        _id
+        username
+        game
+        scoreValue
+        createdAt
+      }
     }
   }
 `;
+
+export const QUERY_SCORES = gql`
+  query getScores {
+    scores {
+      _id
+      username
+      game
+      scoreValue
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_SINGLE_SCORE = gql`
+  query getSingleScore {
+    score {
+      _id
+      username
+      game
+      scoreValue
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_LIKES = gql``; // Figure this out later, need to add "likes" to resolvers and typeDefs
