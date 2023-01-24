03d711c599a8c47296396361fca712e62e79f0a9
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2023-01-24 14:53:51 -0600

    css go back buttons

--- client/src/pages/LightsOut/lightsOut.js
+++ client/src/pages/LightsOut/lightsOut.js
@@ -6,14 +6,14 @@ import { Button } from '@mui/material';
 function Lights() {
   return (
     <div className="AppLights">
-      <Button  sx={{
+      <Board size={5} chanceLightStartsOn={0.25} />
+      <p>TURN ALL THE LIGHTS OFF</p>
+      <Button className='lightsbutton' sx={{
               width: 200,
               fontFamily: "MenloRegular",
               fontSize: 15,
               color: "white",
             }} href='/games'> GO BACK </Button>
-      <Board size={5} chanceLightStartsOn={0.25} />
-      <p>TURN ALL THE LIGHTS OFF</p>
     </div>
   );
 }
