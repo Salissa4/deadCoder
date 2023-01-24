39a24e84979ef30e7d9f6622e3c5d5787c44404d
Monet <shelbyhernandez9876@yahoo.com>
2023-01-23 19:06:23 -0600

    TAILWIND WORKS

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './src/**/**/*.{js,jsx,ts,tsx}'],
   important: '#root',
   theme: {
     extend: {},
